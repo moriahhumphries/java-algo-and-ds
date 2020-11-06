03e0d7228c11aa02054d5c80007488541b063e6b
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-06 15:52:50 -0600

    completed selection sort example

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,24 +1,23 @@
 package sorting;
 
-public class Main {
-    public static void main(String[] args) {
-        int[] intArray = new int[7];
+public class SelectionSort {
 
-        intArray[0] = 20;
-        intArray[1] = 35;
-        intArray[2] = -15;
-        intArray[3] = 7;
-        intArray[4] = 55;
-        intArray[5] = 1;
-        intArray[6] = -22;
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i , i + 1);
+
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
                 }
             }
+
+            swap(intArray, largest, lastUnsortedIndex);
         }
+
 
         for (int i = 0; i <intArray.length; i++) {
             System.out.println(intArray[i]);
