b617e2f41b1ef1a87348b062c8f020d18c26da15
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-12 13:31:40 -0600

    completed merge sort

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -5,9 +5,13 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
+        mergeSort(intArray, 0, intArray.length);
+
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
+
+
     }
 
     public static void mergeSort(int[] input, int start, int end) {
@@ -32,12 +36,13 @@ public class MergeSort {
         int j = mid;
         int tempIndex = 0;
 
-        int[] temp = new int[end = start];
+        int[] temp = new int[end - start];
 
         while (i < mid && j < end) {
             temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
         }
         System.arraycopy(input, i, input,start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
 
     }
 }
