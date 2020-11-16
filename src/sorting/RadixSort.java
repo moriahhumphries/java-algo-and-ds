49ca635d0df4844056b7939234c04084e4fc6cb7
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-16 12:06:57 -0600

    setup for radix sort

---
+++ src/sorting/RadixSort.java
@@ -0,0 +1,11 @@
+package sorting;
+
+public class RadixSort {
+    public static void main(String[] args) {
+        int [] radixArray = {4725, 4586, 1130, 8793, 1594, 5729};
+
+        for (int i = 0; i < radixArray.length; i++) {
+            System.out.println(radixArray[i]);
+        }
+    }
+}
