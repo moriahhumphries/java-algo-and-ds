3045d3a136b65780b2aa8d5a2ac3b124b1ac6e29
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-17 14:45:06 -0600

    completed implementation of radix sort

--- src/sorting/RadixSort.java
+++ src/sorting/RadixSort.java
@@ -4,6 +4,8 @@ public class RadixSort {
     public static void main(String[] args) {
         int [] radixArray = {4725, 4586, 1130, 8793, 1594, 5729};
 
+        radixSort(radixArray, 10, 4);
+
         for (int i = 0; i < radixArray.length; i++) {
             System.out.println(radixArray[i]);
         }
@@ -27,11 +29,18 @@ public class RadixSort {
         for (int j = 1; j < radix; j++) {
             countArray[j] += countArray[j - 1];
         }
+
+        int[] temp = new int[numItems];
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input[tempIndex], radix)]] = input[tempIndex];
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            input[tempIndex] = temp[tempIndex];
+        }
     }
 
     public static int getDigit(int position, int value, int radix) {
-        return value / (int) Math.pow(10, position) % radix;
+        return value / (int) Math.pow(radix, position) % radix;
     }
-
-
 }
