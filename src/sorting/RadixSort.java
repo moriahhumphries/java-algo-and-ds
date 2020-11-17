6b0acd9c1cf22d19be2d991bcf2af271b412beaa
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-17 14:39:38 -0600

    created methods for radix sort

--- src/sorting/RadixSort.java
+++ src/sorting/RadixSort.java
@@ -8,4 +8,30 @@ public class RadixSort {
             System.out.println(radixArray[i]);
         }
     }
+
+    public static void radixSort(int[] input, int radix, int width) {
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSort(int[] input, int position, int radix) {
+
+        int numItems = input.length;
+        int [] countArray = new int[radix];
+
+        for (int value : input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+    }
+
+    public static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(10, position) % radix;
+    }
+
+
 }
