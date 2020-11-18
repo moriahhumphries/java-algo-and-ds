95128cc545d789e565c0a74badc4004e25c50bca
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-18 15:21:03 -0600

    parallelSort method of sorting

--- src/sorting/JDKSort.java
+++ src/sorting/JDKSort.java
@@ -6,7 +6,7 @@ public class JDKSort {
     public static void main(String[] args) {
         int[] intArray = {20, 25, -15, 7, 55, 1 -22};
 
-        Arrays.sort(intArray);
+        Arrays.parallelSort(intArray);
 
         for (int i = 0; i <intArray.length; i++) {
             System.out.println(intArray[i]);
