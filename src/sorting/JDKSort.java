4618dd952761aeaa0d862ead96e12bef0dc6523b
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-18 15:17:22 -0600

    setup for jdk array tools

---
+++ src/sorting/JDKSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class JDKSort {
+    public static void main(String[] args) {
+        int[] intArray = {20, 25, -15, 7, 55, 1 -22};
+
+        Arrays.sort(intArray);
+
+        for (int i = 0; i <intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+    }
+}
