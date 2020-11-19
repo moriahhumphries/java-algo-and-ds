08fd7ea5e011692d2dcba221bfc4e28faaad8181
Moriah Humphries <moriah.humphries@gmail.com>
2020-11-19 16:25:03 -0600

    completed array list testing

--- src/lists/arraylists/Employee.java
+++ src/lists/arraylists/Employee.java
@@ -1,5 +1,7 @@
 package lists.arraylists;
 
+import java.util.Objects;
+
 public class Employee {
 
     private String firstName;
@@ -34,6 +36,21 @@ public class Employee {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                firstName.equals(employee.firstName) &&
+                lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, id);
     }
 
     @Override
--- src/lists/arraylists/Main.java
+++ src/lists/arraylists/Main.java
@@ -7,6 +7,37 @@ public class Main {
     public static void main(String[] args) {
 
         List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
 
+//        System.out.println("");
+//        System.out.println(employeeList.get(1));
+//        System.out.println("");
+//        System.out.println("Is the list empty? " + employeeList.isEmpty());
+
+        employeeList.set(1, new Employee("John", "Adams", 4568));
+
+
+//        System.out.println(employeeList.size());
+
+        employeeList.add(3, new Employee("John", "Doe", 4567));
+
+//        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+//
+//        for (Employee employee : employeeArray) {
+//            System.out.println("Employee array: " + employee);
+//        }
+
+        // Checks for exact same instance
+        // Requires equals method in Employee
+        System.out.println(employeeList.contains(new Employee("Mary", "Smith", 22)));
+
+        System.out.println(employeeList.indexOf(new Employee("John", "Doe", 4567)));
+
+        employeeList.remove(2);
+
+        employeeList.forEach(employee -> System.out.println(employee));
     }
 }
